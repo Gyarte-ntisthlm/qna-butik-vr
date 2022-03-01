5770935c6838455a638b1190161ffe8788eba57a
Zedzee <43708321+Zedzeen@users.noreply.github.com>
2022-03-01 22:50:47 +0100

    init

---
+++ src/Questionnaire.tsx
@@ -0,0 +1,10 @@
+export default function Questionnaire(params:any) {
+    return (
+        <div>
+            <h1>Questionnaire</h1>
+            <p>
+                This is the questionnaire page.
+            </p>
+        </div>
+    );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Questionnaire from './Questionnaire';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} /> 
+      <Route path="/:questionnaireID/:questionnaireSecret" element={<Questionnaire />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
